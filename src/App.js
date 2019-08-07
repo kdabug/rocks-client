a594860dba5c27f161e6ca8b676950e2f1dde138
Mikayda Mills <mills.ma12@gmail.com>
2019-08-07 10:54:45 -0400

    component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import UserProfile from "./components/UserProfile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rockData: [],
+      chakraData: [],
+      propertyData: [],
+      rockName: "",
+      properties: [],
+      rockUse: "",
+      chakra: "",
+      rockUrl: "",
+      userData: null
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    //console.log("THIS IS HANDLE SUBMIT STATE", this.state);
+    this.setState(prevState => ({
+      tweedrData: prevState.tweedrData.push(this.state.formQuery)
+    }));
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <Header userName={this.state.userName} />
+          <Switch>
+            <Route path="/login" render={props => <Login {...props} />} />
+            <Route path="/logout" render={props => <Logout {...props} />} />
+            <Route path="/login" render={props => <Register {...props} />} />
+            <Route
+              path="/user-profile"
+              rendeer={props => <UserProfile {...props} userName={userName} />}
+            />
+          </Switch>
+          <Footer userName={this.state.userName} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
